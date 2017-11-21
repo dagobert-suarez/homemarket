5a760642869c5ee7eca467659955f598a13c552f
D <d>
2017-11-21 15:02:59 -0500

    error

--- controller/views.controller.php
+++ controller/views.controller.php
@@ -26,10 +26,10 @@ class viewsController{
 		require_once "views/modules/registrosuper.php";
 		require_once "views/include/scope.footer.php";
 	}
-	function paral(){
-		require_once "views/include/scope.header.php";	
-		require_once "views/modules/paral.php";
-	}
+	// function paral(){
+	// 	require_once "views/include/scope.header.php";
+	// 	require_once "views/modules/paral.php";
+	// }
 
 
 	function ingreso(){
