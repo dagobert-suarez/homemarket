4120140f4429d9d96bc21e930e455fcb888a59ee
dagobert-suarez <dagobertsuarez@gmail.com>
2017-11-27 11:48:04 -0500

    CAMBIO
    no se

--- views/include/scope.menutop.php
+++ views/include/scope.menutop.php
@@ -1,7 +1,7 @@
 <div class="menu">
-		<!-- <div class="logo--menu"> -->
-				<!-- <img  class="logo--img"src="views/assets/image/logoprueba.jpg" alt=""> -->
-		<!-- </div> -->
+		<div class="logo--menu">
+				<img  class="logo--img"src="views/assets/image/logoprueba.jpg" alt="">
+		</div>
 		<div class="container--menu">
 			<a href="inicio">Inicio</a>
 				<a href="super">Supermerdacos</a>
