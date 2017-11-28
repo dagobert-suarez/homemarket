af0002fa5f6e3e9e03cd402618a2ffa8c432212c
dagobert-suarez <dagobertsuarez@gmail.com>
2017-11-28 12:38:37 -0500

    cambio
    en registrarse, falataban muchas cosas

--- views/modules/registrosuper.php
+++ views/modules/registrosuper.php
@@ -1,76 +1,99 @@
-<section class="setregistrosup" id="regissup">
-    <div class="formpart">
-    	<h1 class="regis">Registrate en homemarket</h1>
-    	<form method="post" action="nuevo-cliente">
-    		<div class="form-gruop">
-    			<label  class=" required" for="">Nit supermercado</label>
-    			<input type="number" name="data[]" placeholder="Nit" required>
-    		</div>
-    	<div class="content-form">
-    		<div class="form-gruop">
-    			<label  class=" required" for="">Nombre supermercado:</label>
-    			<input type="text" name="data[]" placeholder="Nombre supermercado" class="pitp" required>
-    		</div>
-    		<div class="form-gruop">
-    			<label   for="">Direccion supermercado</label>
-    			<input type="text" name="data[]" placeholder="Direcion" class="pitp">
-    		</div>
-    	</div>
-    	<div class="content-form">
-    		<div class="form-gruop">
-    				<label  class=" required" for="">Telefono super</label>
-    				<input type="number" name="data[]" placeholder="apellido" class="pitp" required>
-    		</div>
-    		<div class="form-gruop">
-    					<label  class="required" for="">loc super</label>
-    					<input type="text" name="data[]" placeholder="segundo apellido" class="pitp">
-    		</div>
-            <div class="form-gruop">
-                        <label  class="required" for="">codigo ciudad</label>
-                        <input type="number" name="data[]" placeholder="codigo ciudad" class="pitp">
-            </div>
-    	</div>
-        <button type="submit" name="registrar" class="btntnt">Registrarse</button>
-    </form>
-</div>
+<div class="setregistrarse" id="registro">
+	<div class="formtext ">
+		<h1 class="tlt">
+			EMPIEZA UNA EXPERIENCIA CON HOME MARKET
+		</h1>
+		<div class="text-inf ">
+			<p>Crea tú cuenta y empieza a disfrutar de todos nuestros beneficios.</p>
+		</div>
+		<!-- <a href="registrosuper">registrarse como supermercado</a> -->
 
-</section>
-<div id="regisupmodal" class="regigsup">
-	<form  class="modal-content-sup" method="POST" action="validar" >
-		<div class="container-img">
-			<span id="close" class="close" >&times;</span>
-			<h1 class="title">Registrarse</h1>
+	</div>
+<div class="formpart">
+	<h1 class="regis">Registrate en homemarket</h1>
+	<form id="frmregistro" >
+		<div class="form-gruop">
+			<label  class="required"  for="">Numero de documento:</label>
+			<input type="number" name="data[]" class="new" id="documento" placeholder="documento identidad" required>
 		</div>
-		<div class="container-form">
-			<label class="required" for="txtemail"><b>Nit Supermercado</b></label>
-			<input  type="number" name="data[]" placeholder="Nit" required>
+	<div class="content-form">
+		<div class="form-gruop">
+			<label  class="required"  for="">Nombre:</label>
+			<input type="text" name="data[]" id="nombre" class="pitp new" placeholder="Nombre" required>
 		</div>
-    <div class="content-form">
-      <div class="form-gruop">
-        <label  class=" required" for="">Nombre supermercado:</label>
-        <input type="text" name="data[]" placeholder="Nombre supermercado" class="pitp" required>
-      </div>
-      <div class="form-gruop">
-        <label   for="">Direccion supermercado</label>
-        <input type="text" name="data[]" placeholder="Direcion" class="pitp">
-      </div>
-    </div>
-    <div class="content-form">
-      <div class="form-gruop">
-          <label  class=" required" for="">Telefono super</label>
-          <input type="number" name="data[]" placeholder="apellido" class="pitp" required>
-      </div>
-      <div class="form-gruop">
-            <label  class="required" for="">loc super</label>
-            <input type="text" name="data[]" placeholder="segundo apellido" class="pitp">
-      </div>
-          <div class="form-gruop">
-                      <label  class="required" for="">codigo ciudad</label>
-                      <input type="number" name="data[]" placeholder="codigo ciudad" class="pitp">
-          </div>
-    </div>
-			<div class="botns">
-				<button type="submit" name="registrar"  class="btntntr">Registrarse</button>
-			</div>
+		<div class="form-gruop">
+				<label  class="required"  for="">Apellido</label>
+				<input type="text" name="data[]" id="Apellido" class="pitp new"placeholder="Apellido" required>
+		</div>
+	</div>
+
+	<div class="content-form">
+		<div class="form-gruop">
+			<label  class="required"  for="">	Fecha de nacimiento</label>
+			<input type="date" name="data[]" id="fecha" class="new" >
+		</div>
+		<div class="form-gruop">
+			<label  class="required" id="genero" for="">Genero</label>
+			<select name="data[]" class="new"  placeholder="Genero" >
+				<option value="m">Hombre</option>
+				<option value="f">Mujer</option>
+				<option value="o">Otro</option>
+			</select>
+		</div>
+
+	</div>
+	<div class="content-form">
+		<div class="form-gruop">
+			<label  class="opcional"  for="">celular</label>
+			<input type="number" name="data[]" class="new" id="celular">
+		</div>
+		<div class="form-gruop">
+			<label  class="opcional"  for="">telefono</label>
+			<input type="number" name="data[]" class="new" id="telefono">
+		</div>
+</div>
+	<div class="form-gruop">
+		<label  class="required"  for="">correo</label>
+		<input type="email" name="data[]" class="new" id="correo" placeholder="Correo" >
+	</div>
+
+	<div class="form-gruop">
+			<label  class="required"  for="">ciudad</label>
+			 <select name="data[]" class="city new" id="ciudad">
+			 <option value=""></option>
+				 <?php
+				 foreach ($this->userModel->readCities() as $row){
+					 echo "<option value='$row[0]'>$row[1]</option>";
+				 }
+				 ?>
+			 </select>
+	</div>
+
+	<div class="form-gruop">
+				<label  class="required"  for="">contraseña</label>
+				<input type="password" name="data[]" class="new" id="password" >
+	</div>
+	<div id="mensaje"></div>
+	<div class="form-gruop">
+				<label  class="required" for="">verificar contraseña</label>
+				<input type="password" name="rpassword" id="rpassword" >
+	</div>
+	<div class="form-gruop">
+			<label  class=" required" for="">crear cuenta como</label>
+			 <select name="data[]"  class="city new" >
+			 <option value=""></option>
+				 <?php
+				 foreach ($this->userModel->readRol() as $row){
+					 echo "<option value='$row[0]'>$row[1]</option>";
+				 }
+				 ?>
+			 </select>
+	</div>
+	<div class="fomr-group" id="recaptcha">
+			<div class="g-recaptcha" data-theme="dark" data-sitekey="6LdgiTkUAAAAADv4ddSh-yCAAl-j15tc1K8CAQsv"></div>
+	</div>
+
+		<button type="submit" name="registrar" id="raios" class="btntnt" >Registrarse</button>
 	</form>
+</div>
 </div>
