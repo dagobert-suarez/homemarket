f6914eafa705ec9dd3dce829321280c6b508ac8f
AlexDscrum <amunoz185@misena.edu.co>
2017-11-28 13:30:40 -0500

    cambio
    en el login controller

--- controller/login.controller.php
+++ controller/login.controller.php
@@ -13,7 +13,7 @@ class LoginController{
 	if ($result!=array()){
 		$result = array("true","");
 	}else{
-		$result = array("false","El Usuario no existe");
+		$result = array("false","El Usuario no existe por culpa de chetex");
 	}
 
 	echo json_encode($result);
