2379392737886fafae5d7210706358be5aafbf88
dagobert-suarez <dagobertsuarez@gmail.com>
2017-11-29 15:59:19 -0500

    cambio
    supongo que cambio

--- views/modules/cliente/header.php
+++ views/modules/cliente/header.php
@@ -16,6 +16,7 @@
 
       </div>
       <ul>
+
         <li><i class="fa fa-bell" aria-hidden="true"></i> </li>
         <li><i class="fa fa-cog" aria-hidden="true"></i></li>
         <li> <a href="cerrar-sesion"> <i class="fa fa-sign-out" aria-hidden="true"></i></a></li>
@@ -29,6 +30,5 @@
         require_once 'views/modules/cliente/more.php';
         require_once 'views/modules/cliente/content.gallery.php';
         require_once 'views/modules/cliente/viewProduct.php';
-
       ?>
     </div>
