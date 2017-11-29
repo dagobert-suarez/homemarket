a3f317f1e1a5985ee6090d9cbae7b780a623246a
dagobert-suarez <dagobertsuarez@gmail.com>
2017-11-29 12:36:32 -0500

    añado
    mostrar productos

--- views/modules/cliente/viewProduct.php
+++ views/modules/cliente/viewProduct.php
@@ -1,14 +1,12 @@
 <?php
     $data = $this->viewProducts();
- ?>
+    // print_r($data);
 
-<div class="">
-  <label>nombre_peli:</label>
-      <?php echo $data["nom_pro"];?><br>
-      <label>descripcion: </label>
-      <?php echo $data["des_pro"];?>
-      <label>marca:</label>
-      <?php echo $data["mar_pro"];?><br>
-      <label>categoria: </label>
-      <?php echo $data["nom_tip_pro"];?>
-</div>
+    foreach ($data as $row) {
+      echo $row["nom_pro"];
+       echo $row["des_pro"];
+       echo $row["mar_pro"];
+        echo $row["id_tip_pro"];
+
+    }
+ ?>
