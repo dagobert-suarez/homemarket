10432536d97fec2655666f3f3e7e54051bfebea8
AlexDscrum <amunoz185@misena.edu.co>
2017-12-18 23:15:33 -0500

    admin y iincio

--- views/include/scope.nav.php
+++ views/include/scope.nav.php
@@ -5,10 +5,13 @@
 					<div class="info">
 						<h1 class="infowel">Bienvenido</h1>
 						<p>Home Market es una plataforma donde puedes encontrar</p>
-						<p>supermercados y una cantidad de produtos </p>
+						<p>supermercados y una gran variedad de productos los cuales puedes agregar a tu carrito de compras </p>
+
+						<a class="starbtn" href="#registro">Registrate con Home Market</a>
 					</div>
 			</div>
 	    </div>
+
 		<div class="Slides raz">
 			<img class="img-c" src="views/assets/image/mark.jpg">
 			<div class="text"></div>
