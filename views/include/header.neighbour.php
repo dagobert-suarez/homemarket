74ea92939e01be08dd691d312b715617f479204d
dagobert-suarez <dagobertsuarez@gmail.com>
2018-02-09 13:14:14 -0500

    nuevos cambios en aliados
    ahora sera pagina de explora

--- views/include/header.neighbour.php
+++ views/include/header.neighbour.php
@@ -3,42 +3,11 @@
   <head>
     <meta charset="utf-8">
     <title></title>
-    <link rel="stylesheet" href="../css/neighbour.css">
-    <style>
-    nav, header{
-      display: block;/*sirve para hacer reconocer todos los elementos en bloque*/
-    }
-
-    #menu {
-      background-color: black;
-      padding: 0px 20px;
-    }
-
-    #menu li {
-      display: inline-block;
-      list-style: none;
-      padding: 5px;
-      font: bold 14px verdana, sans-serif;
-    }
-
-    #menu ul li {
-      color: white;
-    }
-
-    #menu ul li:hover {
-      background-color: #eee;
-      color: #0c0c0c;
-      cursor: pointer;
-    }
-
-    #cabeza {
-      background-color: blue;
-    }
-    </style>
+    <link rel="stylesheet" href="views/assets/css/neighbour.css">
   </head>
   <body>
     <header id="cabeza">
-      <h1>pagina de los aliados</h1>
+      <h1>pagina para la capacitacion del cliente</h1>
     </header>
 
     <nav id="menu">
