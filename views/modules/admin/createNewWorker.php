74f8e6f6670f95efc9b77b33914be12b6f601e0a
dagobert-suarez <dagobertsuarez@gmail.com>
2018-03-05 17:47:29 -0500

    mas o menos

--- views/modules/admin/createNewWorker.php
+++ views/modules/admin/createNewWorker.php
@@ -1,1 +1,9 @@
 <h1>la vuelta para crear el nuevo empleado</h1>
+<div class="formWorker">
+  <form class="frmworker" action="index.html" method="post">
+    <label for="">niombre del empleado</label>
+    <input type="text" name="data[]" value="">
+    <label for="">appellido worker</label>
+    <input type="text" name="data[]" value="">
+  </form>
+</div>
