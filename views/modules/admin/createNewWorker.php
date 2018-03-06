3a658e84eda08c44746a48837617589aa886e7be
Davidhzk2 <dherrera035@misena.edu.co>
2018-03-06 15:58:29 -0500

    06/03/2018
    nuevo empleado

--- views/modules/admin/createNewWorker.php
+++ views/modules/admin/createNewWorker.php
@@ -1,9 +1,11 @@
-<h1>la vuelta para crear el nuevo empleado</h1>
-<div class="formWorker">
-  <form class="frmworker" action="index.html" method="post">
-    <label for="">niombre del empleado</label>
-    <input type="text" name="data[]" value="">
-    <label for="">appellido worker</label>
-    <input type="text" name="data[]" value="">
-  </form>
+<div class="">
+  <h1>la vuelta para crear el nuevo empleado</h1>
+  <div class="formWorker">
+    <form class="frmworker" action="index.html" method="post">
+      <label for="">niombre del empleado</label>
+      <input type="text" name="data[]" value="">
+      <label for="">appellido worker</label>
+      <input type="text" name="data[]" value="">
+    </form>
+  </div>
 </div>
