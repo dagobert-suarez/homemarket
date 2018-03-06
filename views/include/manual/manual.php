b3fc2c633b6c47b68027b5f6771d132b9963cec5
dagobert-suarez <dagobertsuarez@gmail.com>
2018-03-06 16:16:17 -0500

    lo de la otra cosa

--- views/include/manual/manual.php
+++ views/include/manual/manual.php
@@ -1,19 +1,29 @@
 <div class="slide-container">
-  <div class="slide">
-    <div class="numero">1/3</div>
+  <div class="mySlides fade">
+    <div class="numbertext">1/3</div>
     <img src="views/assets/image/slide1.jpg" style="width:100%">
     <div class="">tal vez el texto</div>
   </div>
 
-  <div class="slide">
-    <div class="numero">2/3</div>
-    <!-- <img src="" alt=""> -->
+  <div class="mySlides fade">
+    <div class="numbertext">2/3</div>
+    <img src="views/assets/image/slide2.png" style="width:100%">
     <div class="">tal vez el texto 2</div>
   </div>
 
-  <div class="slide">
-    <div class="numero">3/3</div>
-    <!-- <img src="" alt=""> -->
+  <div class="mySlides fade">
+    <div class="numbertext">3/3</div>
+    <img src="views/assets/image/slide3.png" style="width:100%">
     <div class="">tal vez el texto 3</div>
   </div>
+
+<a class="prev" onclick="plusSlide(-1)">&#10094;</a>
+<a class="next" onclick="plusSlide(1)">&#10095;</a>
+</div>
+<br>
+
+<div style="text-align:center">
+  <span class="dot" onclick="currentSlide(1)"></span>
+  <span class="dot" onclick="currentSlide(2)"></span>
+  <span class="dot" onclick="currentSlide(3)"></span>
 </div>
