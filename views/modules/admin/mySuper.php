0fd641d38056012d2e033b773033d90e07ddd739
D <d>
2018-03-20 12:40:07 -0500

    20/03/2018
    El formulario de el Supermercado y del empleado
    
    Co-Authored-By: davidhzk2 <davidhzk2@users.noreply.github.com>

--- views/modules/admin/mySuper.php
+++ views/modules/admin/mySuper.php
@@ -1,4 +1,73 @@
 <div class="module--worker">
 
-    <h1>Aquí la vuelta del supermercado</h1>
+    <h1>Supermercados</h1>
+    <div class="tab">
+        <button class="btntabs" onclick="openCity(event, 'Nuevo')" id="defaultOpen"><i class="fa fa-plus"></i> Nuevo</button>
+        <button class="btntabs" onclick="openCity(event, 'Supermercados')"><i class="fa fa-user"></i> Supermercados</button>
+        <button class="btntabs" onclick="openCity(event, 'Nose')"><i class="fa fa-question"></i> Nose</button>
+      </div>
+
+      <div id="Nuevo" class="contentfo">
+        <h3>Registrar Supermercado</h3>
+        <div class="frmworker">
+          <form class="frmregistro">
+              <div class="content-form">
+                    <div class="form-group">
+                        <label class="required" for="" >Nombre del Supermercado:</label>
+                        <input class="k"  type="text" name="data[]" value="" required>
+                    </div>
+                    <div class="form-group">
+                        <label  class="required" for="">Direccion:</label>
+                        <input  class="k" type="text" name="data[]" value="">
+                    </div>
+              </div>
+              <div class="content-form">
+                  <div class="form-group">
+                      <label  class="required" for="">Teléfono:</label>
+                      <input  class="k" type="number" name="data[]" value="">
+                  </div>
+                   <div class="form-group">
+                      <label  class="required" for="">Ciudad:</label>
+                      <select class="ks" name="data[]">
+                          <?php
+                          foreach ($this->userModel->readCities() as $row){
+                              echo "<option value='$row[0]'>$row[1]</option>";
+                          }
+                          ?>
+                      </select>
+                  </div>
+              </div>
+              <div class="content-form">
+                  <div class="form-group">
+                      <label  class="required" for="">Logo del Supermercado:</label>
+                      <input  class="kfile" type="file" name="data[]" value="">
+                  </div>
+                  <div class="form-group">
+                      <label class="required" for="">Longuitude:</label>
+                      <input  class="k" type="bigint" name="data[]" value="">
+                  </div>
+              </div>
+              <div class="content-form">
+
+                  <div class="form-group">
+                      <label class="required" for="">Latitude:</label>
+                      <input  class="k" type="bigint" name="data[]" value="" class="noda">
+                  </div>
+                  <div class="form-group">
+                      <button type="button" name="button" class="btntnt">Registrar</button>
+                  </div>
+              </div>
+          </form>
+          <!-- <h1>the record is missing</h1> -->
+        </div>
+      </div>
+
+      <div id="Supermercados" class="contentfo">
+        <h3>Supermercados</h3>
+
+      </div>
+
+      <div id="Nose" class="contentfo">
+        <h3>Nose</h3>
+      </div>
 </div>
