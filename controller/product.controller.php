97bd5229a61101f162b1af61152cb979e8abad3b
D <d>
2018-04-05 13:21:31 -0500

    05/04/2018
    producto
    
    Co-Authored-By: davidhzk2 <davidhzk2@users.noreply.github.com>

--- controller/product.controller.php
+++ controller/product.controller.php
@@ -54,42 +54,42 @@ class ProductController{
     function crearProduct(){
         $data = $_POST['data'];
 
-        if (isset($_FILES["file"]) && $_FILES["file"]["error"] == 0){
-              $allowed = array("jpg"=>"image/jpg", "jpeg"=>"image/jpeg","png"=>"image/png", "gif"=>"image/gif");
-              $filetype = $_FILES["file"]["type"];
-              $filesize = $_FILES["file"]["size"];
-              $extention = pathinfo($_FILES["file"]["name"]);
-              $extention = ".".$extention["extension"];
-
-              $var_rand = rand(10000,999999) * rand(10000,999999);
-              $tmp_name = md5($var_rand.$_FILES["file"]["name"]);
-
-              $filename = $tmp_name.$extention;
-
-              $extention = pathinfo($filename, PATHINFO_EXTENSION);
-
-              if (!array_key_exists($extention, $allowed)) {
-                die("Error: favor seleccione un formato valido (.jpg, .png, .gif, jpeg)");
-              }
-              $maxsize= 2 * 1024 * 1024;
-              if($filesize > $maxsize){
-                die("Error: el tamaño del archivo debe ser menor o igual a 2MB");
-              }
-              //mine type
-              if (in_array($filetype, $allowed)) {
-                if (file_exists("views/assets/image/productos/".$filename)) {
-                die("lo sentimos ese archivo ya existe");
-              }else{
-                  move_uploaded_file($_FILES["file"]["tmp_name"],"views/assets/image/productos/".$filename);
-                  $data[4] = $filename;
-                }
-            }else{
-              die("Error: no se puede reconocer la imagen intente nuevamente");
-            }
-
-        }else {
-          die("Error ".$_FILE["file"]["error"]);
-        }
+        // if (isset($_FILES["file"]) && $_FILES["file"]["error"] == 0){
+        //       $allowed = array("jpg"=>"image/jpg", "jpeg"=>"image/jpeg","png"=>"image/png", "gif"=>"image/gif");
+        //       $filetype = $_FILES["file"]["type"];
+        //       $filesize = $_FILES["file"]["size"];
+        //       $extention = pathinfo($_FILES["file"]["name"]);
+        //       $extention = ".".$extention["extension"];
+        //
+        //       $var_rand = rand(10000,999999) * rand(10000,999999);
+        //       $tmp_name = md5($var_rand.$_FILES["file"]["name"]);
+        //
+        //       $filename = $tmp_name.$extention;
+        //
+        //       $extention = pathinfo($filename, PATHINFO_EXTENSION);
+        //
+        //       if (!array_key_exists($extention, $allowed)) {
+        //         die("Error: favor seleccione un formato valido (.jpg, .png, .gif, jpeg)");
+        //       }
+        //       $maxsize= 2 * 1024 * 1024;
+        //       if($filesize > $maxsize){
+        //         die("Error: el tamaño del archivo debe ser menor o igual a 2MB");
+        //       }
+        //       //mine type
+        //       if (in_array($filetype, $allowed)) {
+        //         if (file_exists("views/assets/image/productos/".$filename)) {
+        //         die("lo sentimos ese archivo ya existe");
+        //       }else{
+        //           move_uploaded_file($_FILES["file"]["tmp_name"],"views/assets/image/productos/".$filename);
+        //           $data[4] = $filename;
+        //         }
+        //     }else{
+        //       die("Error: no se puede reconocer la imagen intente nuevamente");
+        //     }
+        //
+        // }else {
+        //   die("Error ".$_FILE["file"]["error"]);
+        // }
          $result = $this->product->createProduct($data);
          echo '<script language="javascript">alert("creado con exito");</script>';
          echo "<script>window.location.href='nuevo-producto'</script>";
