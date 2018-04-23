f4d3ac054d763d70373cc1dec25bdb3189c8447f
AlexDscrum <amunoz185@misena.edu.co>
2018-04-23 17:52:48 -0500

    change

--- views/include/manual/modules/info.php
+++ views/include/manual/modules/info.php
@@ -5,21 +5,21 @@
        <div class="uno">
            <i class="fa fa-user-plus"></i>
            <h2>Registro</h2>
-           <a href="modulo1-r1">¿Pregunta 1?</a>
-           <a href="modulo1-r2">¿Pregunta 2?</a>
-           <a href="modulo1-r3">¿Pregunta 3?</a>
-           <a href="modulo1-r4">¿Pregunta 4?</a>
-           <a href="modulo1-r5">¿Pregunta 5?</a>
+           <a href="modulo1-r1">¿Como puedo crear un cuenta?</a>
+           <a href="modulo1-r2">¿Como inicio mi cuenta?</a>
+           <a href="modulo1-r3">¿Como puedo configurar mi cuenta?</a>
+           <a href="modulo1-r4">¿Como puedo recuperar mi cuenta si perdi mi contraseña?</a>
+           <a href="modulo1-r5">¿Como elimino mi cuenta?</a>
 
        </div>
        <div class="dos">
            <i class="fa fa-cart-arrow-down" aria-hidden="true"></i>
            <h2>Pedidos</h2>
-           <a href="modulo2-r1">¿Pregunta 1?</a>
-           <a href="modulo2-r2">¿Pregunta 2?</a>
-           <a href="modulo2-r3">¿Pregunta 3?</a>
-           <a href="modulo2-r4">¿Pregunta 4?</a>
-           <a href="modulo2-r5">¿Pregunta 5?</a>
+           <a href="modulo4">¿Como hacer un pedido?</a>
+           <a href="modulo4">¿Como saber la cobertura del supermercado donde hago mi pedido?</a>
+           <a href="modulo4">¿Cual es la forma de pago de un pedido?</a>
+           <a href="modulo4">¿Donde puedo ver el historial de mis pedidos?</a>
+           <!-- <a href="modulo2-r5">¿Pregunta 5?</a> -->
 
        </div>
      </div>
@@ -27,11 +27,11 @@
        <div class="tres">
            <i class="fa fa-building-o"></i>
            <h2>Supermercado</h2>
-           <a href="modulo2">¿Pregunta 1?</a>
-           <a href="modulo2">¿Pregunta 2?</a>
-           <a href="modulo2">¿Pregunta 3?</a>
-           <a href="modulo2">¿Pregunta 4?</a>
-           <a href="modulo2">¿Pregunta 5?</a>
+           <a href="modulo2-r1">¿En donde puedo ver los supermercados?</a>
+           <a href="modulo2-r2">¿Como puedo buscar un supermercado?</a>
+           <a href="modulo2-r3">¿Pregunta 3?</a>
+           <a href="modulo2-r4">¿Pregunta 4?</a>
+           <a href="modulo2-r5">¿Pregunta 5?</a>
 
        </div>
        <div class="cuatro">
@@ -49,9 +49,9 @@
        <div class="uno">
            <i class="fa fa-shopping-basket"></i>
            <h2>Productos</h2>
-           <a href="modulo3">¿Pregunta 1?</a>
-           <a href="modulo3">¿Pregunta 2?</a>
-           <a href="modulo3">¿Pregunta 3?</a>
+           <a href="modulo3">¿Donde puedo ver los productos?</a>
+           <a href="modulo3">¿Como puedo buscar un producto?</a>
+           <a href="modulo3">¿Como puedo filtrar un producto?</a>
            <a href="modulo3">¿Pregunta 4?</a>
            <a href="modulo3">¿Pregunta 5?</a>
        </div>
