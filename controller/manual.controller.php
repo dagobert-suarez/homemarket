94d60b7df834bcfb406c936f54de75c33c7ddc3a
dagobert-suarez <dagobertsuarez@gmail.com>
2018-04-23 20:46:47 -0500

    url amigable

--- controller/manual.controller.php
+++ controller/manual.controller.php
@@ -1,6 +1,6 @@
 <?php
  class manualController{
-     // manual de usuario 
+     // manual de usuario
  	function explora(){
  		require_once "views/include/manual/header.php";
  		require_once "views/include/manual/modules/info.php";
@@ -14,12 +14,12 @@
  	}
   function mo1res1(){
     require_once "views/include/manual/header.php";
-    require_once "views/include/manual/respuestas/mod1.respuesta1.php";
+    require_once "views/include/manual/respuestas/mod1-respuesta1.php";
     require_once "views/include/manual/footer.php";
   }
   function mo1res2(){
     require_once "views/include/manual/header.php";
-    require_once "views/include/manual/respuestas/mod1.respuesta2.php";
+    require_once "views/include/manual/respuestas/mod1-respuesta2.php";
     require_once "views/include/manual/footer.php";
   }
    function mo1res3(){
