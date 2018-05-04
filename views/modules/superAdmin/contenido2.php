016340ebb414e400b7ed4f84ad8e67dd24e39db2
Davidhzk2 <dherrera035@misena.edu.co>
2018-05-04 13:41:53 -0500

    04/05/2018
    Huevo de pascua

--- views/modules/superAdmin/contenido2.php
+++ views/modules/superAdmin/contenido2.php
@@ -1,5 +1,6 @@
 <h1>Contenido 2</h1>
-
+<p>Si vas Exportar e Importar La base de datos debes ingresar la contraseña (La que fue guardada).</p>
+<p>Si no Sabes la Contraseña  es: <b>*******</b></p>
 <input type="password" name="campo" value="" id="pass">
 <button type="button" name="button" id="content">Mostar-Contenido</button>
 <div class="botons">
