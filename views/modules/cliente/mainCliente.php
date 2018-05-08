5317e939bec1e06bae83660f9a0f4385c1eadf69
Davidhzk2 <dherrera035@misena.edu.co>
2018-05-08 17:45:04 -0500

    08/05/2018
    mapa

--- views/modules/cliente/mainCliente.php
+++ views/modules/cliente/mainCliente.php
@@ -1,6 +1,9 @@
 <div class="showcontent" >
   <div class="mapa">
-   <div id="map"></div>
+   <div id="map">nose</div>
   </div>
   <div class="nose"> hola soy el mapa </div>
+
+  <!-- <?php  require_once "views/modules/cliente/viewSuper.php";?> -->
+
 </div>
