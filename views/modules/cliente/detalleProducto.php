7e74f47b9e514deb0f946f982651f6301f32ff02
dagobert-suarez <dagobertsuarez@gmail.com>
2018-05-16 15:36:24 -0500

    sizas en la imagen

--- views/modules/cliente/detalleProducto.php
+++ views/modules/cliente/detalleProducto.php
@@ -3,7 +3,7 @@
   <h1>aquí van los detalles del producto</h1>
   <div class="">
     <label for=""><?php echo $dataProduct["nom_pro"]; ?></label>
-    <label for=""><?php echo $dataProduct["img_pro"]; ?></label>
+    <img src="views/assets/image/productos/<?php echo $row["img_pro"]; ?>" alt="">
     <label for=""><?php echo $dataProduct["mar_pro"]; ?></label>
     <label for=""><?php echo $dataProduct["valVen_pro"]; ?></label>
   </div>
