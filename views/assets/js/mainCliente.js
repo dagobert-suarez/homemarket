f9e53299cf073b77d507a4ee3a470c8272d81cb7
Davidhzk2 <dherrera035@misena.edu.co>
2018-05-18 15:46:22 -0500

    18/05/2018
    Vista de productos en el supermercado

--- views/assets/js/mainCliente.js
+++ views/assets/js/mainCliente.js
@@ -8,12 +8,30 @@ var closemenu = document.getElementById('closemenu');
 menu.onclick = function(){
   ground.style.display = "block";
   modal.style.display = "block";
-}
+};
 closemenu.onclick = function(){
   ground.style.display = "none";
   modal.style.display = "none";
-}
+};
 ground.onclick = function(){
   ground.style.display = "none";
   modal.style.display = "none";
+};
+
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
