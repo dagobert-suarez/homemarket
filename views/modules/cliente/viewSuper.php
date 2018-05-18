18bd62d2deb402297e45fb8feff65dff811fc2c4
Davidhzk2 <dherrera035@misena.edu.co>
2018-05-18 17:38:56 -0500

    18/05/2018
    Vista de Supermecados en el cliente

--- views/modules/cliente/viewSuper.php
+++ views/modules/cliente/viewSuper.php
@@ -1,22 +1,30 @@
-<div id="map"> </div>
-<?php require_once "views/modules/cliente/navigator.php"; ?>
-<div class="gallery--content">
+<!-- <?php require_once "views/modules/cliente/navigator.php"; ?> -->
+<div class="wrap--map">
+  <div id="map"> </div>
+</div>
+<div class="wrap--supers">
+  <div class="wrap-title">
+    <h3>Supermercados</h3>
     <div class="buscador">
-        <input  class="sch" type="search" name="" value="" placeholder="Buscar Supermercados">
+      <label for="">Buscar Supermercado:</label>
+      <input  class="k" type="search" name="" value="" placeholder="Buscar Supermercados">
     </div>
-    <?php
-    $item =1;
-    foreach ($this->readAllSup() as $row){?>
-        <div class="card" >
-          <img class="card-img-top" src="views/assets/image/super/<?php echo $row["logo_sup"];?>" alt="Logo del Supermercado">
-          <div class="card-body">
-            <h5 class="card-title"><?php echo $row["nom_sup"]; ?></h5>
-            <p class="card-text"><?php echo $row["dir_sup"]; ?></p>
-            <a href="PorSupermercado-<?php echo $row["id_sup"]; ?>" class="btn-go">ir a mercar</a>
+  </div>
+  <div class="gallery--content">
+      <?php
+      $item =1;
+      foreach ($this->readAllSup() as $row){?>
+          <div class="card" >
+            <img class="card-img-top" src="views/assets/image/super/<?php echo $row["logo_sup"];?>" alt="Logo del Supermercado">
+            <div class="card-body">
+              <h5 class="card-title"><?php echo $row["nom_sup"]; ?></h5>
+              <p class="card-text"><?php echo $row["dir_sup"]; ?></p>
+              <a href="PorSupermercado-<?php echo $row["id_sup"]; ?>" class="btn-go">ir a mercar</a>
+            </div>
           </div>
-        </div>
-    <?php
-    $item++;
-    }
-    ?>
+      <?php
+      $item++;
+      }
+      ?>
+  </div>
 </div>
