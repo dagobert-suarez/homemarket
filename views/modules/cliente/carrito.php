4123445740a62048a10e9d13484472b28abe4b07
dagobert-suarez <dagobertsuarez@gmail.com>
2018-06-14 16:22:41 -0500

    ujum sizas

--- views/modules/cliente/carrito.php
+++ views/modules/cliente/carrito.php
@@ -1,29 +1,29 @@
-<?php
-    $data = $this->readbyProduct();
-?>
-<h2>este es el carrito de compras</h2>
-<i class="fa fa-cart-plus"></i>
-<section>
   <?php
-  if (isset($_SESSION['add_cart'])) {
-    $_SESSION['CARRO'][]=array("id"=>$_SESSION['add_cart']['id'],"nombre"=>$_SESSION['add_cart']['nombre'],"cantidad"=>$_SESSION['add_cart']['cantidad'],"precio"=>$_SESSION['add_cart']['precio']);
-    unset($_SESSION['add_cart']);
-  }
-    if (isset($_SESSION['CARRO'])) {
-      $datos=$_SESSION['CARRO'];
-      $total=0;
-      for ($i=0; $i <count($datos); $i++) {
-    ?>
-    <div class="">
-      <span><?php echo $datos[$i]['nombre']; ?></span>
-      <span><?php echo $datos[$i]['precio']; ?></span>
-    </div>
+      $data = $this->readbyProduct();
+  ?>
+  <h2>este es el carrito de compras</h2>
+  <i class="fa fa-cart-plus"></i>
+  <section>
     <?php
-      }
-    }else {
-      echo "<h2>carrito de compras vacio</h2>";
+    if (isset($_SESSION['add_cart'])) {
+      $_SESSION['CARRO'][]=array("id"=>$_SESSION['add_cart']['id'],"nombre"=>$_SESSION['add_cart']['nombre'],"cantidad"=>$_SESSION['add_cart']['cantidad'],"precio"=>$_SESSION['add_cart']['precio']);
+      unset($_SESSION['add_cart']);
     }
-    echo $_POST['cantidad'];
-    echo $_POST['id'];
+      if (isset($_SESSION['CARRO'])) {
+        $datos=$_SESSION['CARRO'];
+        $total=0;
+        for ($i=0; $i <count($datos); $i++) {
       ?>
-</section>
+      <div class="">
+        <span><?php echo $datos[$i]['nombre']; ?></span>
+        <span><?php echo $datos[$i]['precio']; ?></span>
+      </div>
+      <?php
+        }
+      }else {
+        echo "<h2>carrito de compras vacio</h2>";
+      }
+      echo $_POST['cantidad'];
+      echo $_POST['id'];
+        ?>
+  </section>
