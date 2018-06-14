e2977e8ce2536470aa65599202e12447d5038e03
Davidhzk2 <dherrera035@misena.edu.co>
2018-06-14 14:35:47 -0500

    14/06/2018
    js

--- views/assets/js/master.js
+++ views/assets/js/master.js
@@ -23,6 +23,110 @@
   )
   } );
 
+  // modal de la categoria
+  // ----------------------
+  // $(".fondo").css("display","block");
+  $(".abrirmodal").click(function() {
+   $(".fondo").toggle();
+   $(".wrap").toggle();
+   $.ajax({
+     url:"ver-categoria",
+     type:"post",
+     dataType:"json",
+     data:({data:this.id}),
+     success:function(result){
+       console.log(result);
+       $("#updateCategoryName").val(result[1]);
+     },
+     error:function(result){console.log(result);}
+   });
+  });
+  $(".close").click(function(){
+      $(".fondo").toggle();
+      $(".wrap").toggle();
+      $("#updateCategoryName").val("");
+  });
+  //actualizar categoria
+  $("#updateCate").submit(function(e){
+    e.preventDefault();
+    if ($("#updateCategoryName").val()!="") {
+      $.ajax({
+        url:"actualizar-categoria",
+        type:"post",
+        dataType:"json",
+        data:({data:$("#updateCategoryName").val()}),
+        success:function(result){
+          console.log(result);
+          if(result==true){
+            location.reload();
+          }else{
+            alert(result);
+          }
+        },
+        error:function(result){console.log(result);}
+      });
+    }else{
+      alert("Campos vacios");
+    }
+  });
+  $(".fondo").click(function() {
+   // $(".fondo").css("display","none");
+   $(".fondo").toggle();
+   $(".wrap").toggle();
+  });
+  // Fin de la modal de la categoria
+  // --------------------------------
+
+  // Modal de Detalles
+  // -----------------------
+  $(".modalDet").click(function(){
+      var cate = this.id;
+      // id = id.replace("D","");
+      $.ajax({
+        url:"ver-categoria",
+        type:"post",
+        dataType:"json",
+        data:({data:cate}),
+        success:function(result){
+          console.log(result);
+          $(".fondo-det").toggle();
+          $(".wrap-det").toggle();
+          $("#codeCat").html(result.id_tip_pro);
+          $("#nomCat").html(result.nom_tip_pro);
+        },
+        error:function(result){console.log(result);}
+      });
+  });
+  //eliminar categoria
+  function eliminarCategoria(id){
+    if (confirm("¿Eliminar esta categoria?")) {
+      $.ajax({
+        url:"eliminar-categoria",
+        type:"post",
+        dataType:"json",
+        data:({data:id}),
+        success:function(result){
+          console.log(result);
+          if (result==true) {
+            location.reload();
+          }else{
+            alert("Ocurrio un error");
+          }
+        },
+        error:function(result){console.log(result);}
+      });
+    }
+  }
+  $(".closedet").click(function(){
+      $(".fondo-det").toggle();
+      $(".wrap-det").toggle();
+  });
+  $(".fondo-det").click(function() {
+      $(".fondo-det").toggle();
+      $(".wrap-det").toggle();
+  });
+
+
 var contraseña = "nosevea";
 var contraseñaUsu = "homemarket";
 var usu1 = "Alexis";
