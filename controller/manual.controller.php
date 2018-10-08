7b63dac5a025bc7c6dc9af589eb02d65ed4ceaaf
dagobert-suarez <dagobertsuarez@gmail.com>
2018-10-08 08:23:33 -0500

    ujum

--- controller/manual.controller.php
+++ controller/manual.controller.php
@@ -1,112 +1,107 @@
 <?php
  class manualController{
-     // manual de usuario
- 	function explora(){
- 		require_once "views/include/manual/header.php";
- 		require_once "views/include/manual/modules/info.php";
- 		require_once "views/include/manual/footer.php";
- 	}
+     // vistas del manual de usuario
+   function esomismo(){
+
+   }
+ 	 function explora(){
+ 		 require_once "views/include/manual/header.php";
+ 		 require_once "views/include/manual/modules/info.php";
+ 		 require_once "views/include/manual/footer.php";
+ 	 }
   // modulo 1
- 	function lorem(){
- 		require_once "views/include/manual/header.php";
- 		require_once "views/include/manual/modules/lorem.php";
- 		require_once "views/include/manual/footer.php";
- 	}
-  function mo1res1(){
-    require_once "views/include/manual/header.php";
-    require_once "views/include/manual/respuestas/mod1Respuesta1.php";
-    require_once "views/include/manual/footer.php";
-  }
-  function mo1res2(){
-    require_once "views/include/manual/header.php";
-    require_once "views/include/manual/respuestas/mod1.respuesta2.php";
-    require_once "views/include/manual/footer.php";
-  }
+ 	 function lorem(){
+ 		 require_once "views/include/manual/header.php";
+ 		 require_once "views/include/manual/modules/lorem.php";
+ 		 require_once "views/include/manual/footer.php";
+ 	 }
+   function mo1res1(){
+     require_once "views/include/manual/header.php";
+     require_once "views/include/manual/respuestas/mod1Respuesta1.php";
+     require_once "views/include/manual/footer.php";
+   }
+   function mo1res2(){
+     require_once "views/include/manual/header.php";
+     require_once "views/include/manual/respuestas/mod1.respuesta2.php";
+     require_once "views/include/manual/footer.php";
+   }
    function mo1res3(){
-      require_once "views/include/manual/header.php";
-      require_once "views/include/manual/respuestas/mod1.respuesta3.php";
-      require_once "views/include/manual/footer.php";
+     require_once "views/include/manual/header.php";
+     require_once "views/include/manual/respuestas/mod1.respuesta3.php";
+     require_once "views/include/manual/footer.php";
     }
     function mo1res4(){
-       require_once "views/include/manual/header.php";
-       require_once "views/include/manual/respuestas/mod1.respuesta4.php";
-       require_once "views/include/manual/footer.php";
-     }
-     function mo1res5(){
-         require_once "views/include/manual/header.php";
-         require_once "views/include/manual/respuestas/mod1.respuesta5.php";
-         require_once "views/include/manual/footer.php";
-       }
-       // --------------
+      require_once "views/include/manual/header.php";
+      require_once "views/include/manual/respuestas/mod1.respuesta4.php";
+      require_once "views/include/manual/footer.php";
+    }
+    function mo1res5(){
+      require_once "views/include/manual/header.php";
+      require_once "views/include/manual/respuestas/mod1.respuesta5.php";
+      require_once "views/include/manual/footer.php";
+    }
   // modulo2
- 	function modulo2(){
- 		require_once "views/include/manual/header.php";
- 		require_once "views/include/manual/modules/modulo2.php";
- 		require_once "views/include/manual/footer.php";
- 	}
-
-  function mo2res2(){
-    require_once "views/include/manual/header.php";
-    require_once "views/include/manual/respuestas/mod2.respuesta2.php";
-    require_once "views/include/manual/footer.php";
-  }
-   function mo2res3(){
+ 	  function modulo2(){
+ 		  require_once "views/include/manual/header.php";
+ 		  require_once "views/include/manual/modules/modulo2.php";
+ 		  require_once "views/include/manual/footer.php";
+ 	  }
+    function mo2res2(){
+      require_once "views/include/manual/header.php";
+      require_once "views/include/manual/respuestas/mod2.respuesta2.php";
+      require_once "views/include/manual/footer.php";
+    }
+    function mo2res3(){
       require_once "views/include/manual/header.php";
       require_once "views/include/manual/respuestas/mod2.respuesta3.php";
       require_once "views/include/manual/footer.php";
     }
-  // ------------
   // Modulo 3
- 	function modulo3(){
- 		require_once "views/include/manual/header.php";
- 		require_once "views/include/manual/modules/modulo3.php";
- 		require_once "views/include/manual/footer.php";
- 	}
-  function mo3res2(){
-       require_once "views/include/manual/header.php";
-       require_once "views/include/manual/respuestas/mod3.respuesta2.php";
-       require_once "views/include/manual/footer.php";
+ 	  function modulo3(){
+ 		  require_once "views/include/manual/header.php";
+ 		  require_once "views/include/manual/modules/modulo3.php";
+ 		  require_once "views/include/manual/footer.php";
+ 	  }
+    function mo3res2(){
+      require_once "views/include/manual/header.php";
+      require_once "views/include/manual/respuestas/mod3.respuesta2.php";
+      require_once "views/include/manual/footer.php";
     }
     function mo3res3(){
       require_once "views/include/manual/header.php";
       require_once "views/include/manual/respuestas/mod3.respuesta3.php";
       require_once "views/include/manual/footer.php";
     }
-  // --------------
   // modulo4
-
- 	function modulo4(){
- 		require_once "views/include/manual/header.php";
- 		require_once "views/include/manual/modules/modulo4.php";
- 		require_once "views/include/manual/footer.php";
- 	}
-  function mo4res2(){
-    require_once "views/include/manual/header.php";
-    require_once "views/include/manual/respuestas/mod4.respuesta2.php";
-    require_once "views/include/manual/footer.php";
-  }
-  function mo4res3(){
-    require_once "views/include/manual/header.php";
-    require_once "views/include/manual/respuestas/mod4.respuesta3.php";
-    require_once "views/include/manual/footer.php";
-  }
-  function mo4res4(){
+ 	  function modulo4(){
+ 		  require_once "views/include/manual/header.php";
+ 		  require_once "views/include/manual/modules/modulo4.php";
+ 		  require_once "views/include/manual/footer.php";
+ 	  }
+    function mo4res2(){
+      require_once "views/include/manual/header.php";
+      require_once "views/include/manual/respuestas/mod4.respuesta2.php";
+      require_once "views/include/manual/footer.php";
+    }
+    function mo4res3(){
+      require_once "views/include/manual/header.php";
+      require_once "views/include/manual/respuestas/mod4.respuesta3.php";
+      require_once "views/include/manual/footer.php";
+    }
+    function mo4res4(){
       require_once "views/include/manual/header.php";
       require_once "views/include/manual/respuestas/mod4.respuesta4.php";
       require_once "views/include/manual/footer.php";
     }
-  // -------
- 	function modulo5(){
- 		require_once "views/include/manual/header.php";
- 		require_once "views/include/manual/modules/modulo5.php";
- 		require_once "views/include/manual/footer.php";
- 	}
- 	function modulo6(){
- 		require_once "views/include/manual/header.php";
- 		require_once "views/include/manual/modules/modulo6.php";
- 		require_once "views/include/manual/footer.php";
- 	}
-
- }
-
- ?>
+ 	  function modulo5(){
+ 		  require_once "views/include/manual/header.php";
+ 		  require_once "views/include/manual/modules/modulo5.php";
+ 		  require_once "views/include/manual/footer.php";
+ 	  }
+ 	  function modulo6(){
+ 		  require_once "views/include/manual/header.php";
+ 		  require_once "views/include/manual/modules/modulo6.php";
+ 		  require_once "views/include/manual/footer.php";
+ 	  }
+}
+?>
